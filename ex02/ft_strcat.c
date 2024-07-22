c4a39fbb0ede9a0c4559d7eb20335084213389ed
crjarill <jarijarilopez@gmail.com>
2024-07-22 14:51:37 +0200

    coso

--- ex02/ft_strcat.c
+++ ex02/ft_strcat.c
@@ -6,7 +6,7 @@
 /*   By: crjarill <crjarill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 16:09:35 by crjarill          #+#    #+#             */
-/*   Updated: 2024/07/17 16:52:56 by crjarill         ###   ########.fr       */
+/*   Updated: 2024/07/22 14:49:24 by crjarill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,22 @@
 #include <string.h>
 #include <stdio.h>
 
-char *ft_strcat(char *dest, char *src)
+char	*ft_strcat(char *dest, char *src)
 {
-	
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	while (dest[j] != '\0')
+	{
+		dest[i+j] = src[i];
+		i++;
+		j++;
+	}
+	return (*dest);
 }
